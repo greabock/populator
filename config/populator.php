1d2e16ba3701a48ecc876c6250dbb29025609102
Roman Sokharev <greabock@gmail.com>
2019-07-24 16:07:04 +0300

    Update Provider and package discovery

---
+++ config/populator.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'key_generator' => \Greabock\Populator\UuidGenerator::class,
+];
--- src/PopulatorServiceProvider.php
+++ src/PopulatorServiceProvider.php
@@ -9,9 +9,16 @@ class PopulatorServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton(KeyGeneratorInterface::class, UuidGenerator::class);
+        $this->mergeConfigFrom(__DIR__ . '/../config/populator.php', 'populator');
+
+        $this->app->singleton(KeyGeneratorInterface::class, $this->app['config']['populator.key_generator']);
         $this->app->singleton(UnitOfWork::class);
         $this->app->singleton(IdentityMap::class);
         $this->app->singleton(Populator::class);
+    }
+
+    public function boot()
+    {
+        $this->publishes([__DIR__ . '/../config/populator.php' => config_path('populator.php')]);
     }
 }
