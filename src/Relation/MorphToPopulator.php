17b221c7dc9e3f0dd6243b8c3db7aed26ddfc03d
Роман Сохарев <greabock@gmail.com>
2021-07-16 20:36:37 +0700

    Realtion name fix

--- src/Relation/MorphToPopulator.php
+++ src/Relation/MorphToPopulator.php
@@ -23,7 +23,7 @@ class MorphToPopulator extends RelationPopulator
 
         $this->fillRelationField($model, $relation, $related);
 
-        $model->setRelation(Str::snake($relationName)), $related);
+        $model->setRelation(Str::snake($relationName), $related);
     }
 
     protected function fillRelationField(Model $model, BelongsTo $relation, ?Model $related): void
