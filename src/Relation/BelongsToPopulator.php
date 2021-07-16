d26a1e46db787aea0599fa14435f35c0d1938411
Роман Сохарев <greabock@gmail.com>
2021-07-16 20:31:05 +0700

    Direct relation name instead of extracting it

--- src/Relation/BelongsToPopulator.php
+++ src/Relation/BelongsToPopulator.php
@@ -21,7 +21,7 @@ class BelongsToPopulator extends RelationPopulator
 
         $this->fillRelationField($model, $relation, $related);
 
-        $model->setRelation(Str::snake($relation->getRelationName()), $related);
+        $model->setRelation(Str::snake($relationName)), $related);
     }
 
     protected function fillRelationField(Model $model, BelongsTo $relation, ?Model $related): void
